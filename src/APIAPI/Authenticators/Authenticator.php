a6e119a2ce08f82e0d22950cae55856eeb1d4f0d
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-01-25 14:03:05 +0100

    Improve Authenticator base class to contain more abstract logic.

--- src/APIAPI/Authenticators/Authenticator.php
+++ src/APIAPI/Authenticators/Authenticator.php
@@ -9,6 +9,7 @@
 
 namespace awsmug\APIAPI\Authenticators;
 
+use awsmug\APIAPI\Util;
 use awsmug\APIAPI\Name_Trait;
 
 if ( ! class_exists( 'awsmug\APIAPI\Authenticators\Authenticator' ) ) {
@@ -24,6 +25,15 @@ if ( ! class_exists( 'awsmug\APIAPI\Authenticators\Authenticator' ) ) {
 		use Name_Trait;
 
 		/**
+		 * Default authentication arguments.
+		 *
+		 * @since 1.0.0
+		 * @access protected
+		 * @var array
+		 */
+		protected $default_args = array();
+
+		/**
 		 * Constructor.
 		 *
 		 * @since 1.0.0
@@ -33,7 +43,29 @@ if ( ! class_exists( 'awsmug\APIAPI\Authenticators\Authenticator' ) ) {
 		 */
 		public function __construct( $name ) {
 			$this->set_name( $name );
+			$this->set_default_args();
 		}
+
+		/**
+		 * Parses request authentication data.
+		 *
+		 * @since 1.0.0
+		 * @access protected
+		 *
+		 * @param awsmug\APIAPI\Request\Request $request The request to send.
+		 * @return array Parsed authentication data for the request.
+		 */
+		protected function parse_authentication_data( $request ) {
+			return Util::parse_args( $request->get_authentication_data(), $this->default_args );
+		}
+
+		/**
+		 * Sets the default authentication arguments.
+		 *
+		 * @since 1.0.0
+		 * @access protected
+		 */
+		protected abstract function set_default_args();
 	}
 
 }
