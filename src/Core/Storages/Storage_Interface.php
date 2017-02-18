8b19b633a41e692940c692753d0698505fbd6d64
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-02-18 15:32:50 +0100

    Use better method names for storage methods, implement Array_Storage base class.

--- src/Core/Storages/Storage_Interface.php
+++ src/Core/Storages/Storage_Interface.php
@@ -30,7 +30,7 @@ if ( ! interface_exists( 'APIAPI\Core\Storages\Storage_Interface' ) ) {
 		 * @param string $key      The key to store a value for.
 		 * @param scalar $value    The value to store.
 		 */
-		public function set( $basename, $group, $key, $value );
+		public function store( $basename, $group, $key, $value );
 
 		/**
 		 * Stores multiple values.
@@ -42,7 +42,7 @@ if ( ! interface_exists( 'APIAPI\Core\Storages\Storage_Interface' ) ) {
 		 * @param string $group           The group identifier of the group in which to store.
 		 * @param array  $keys_and_values Associative array of `$key => $value` pairs.
 		 */
-		public function set_multi( $basename, $group, $keys_and_values );
+		public function store_multi( $basename, $group, $keys_and_values );
 
 		/**
 		 * Retrieves a single value.
@@ -55,7 +55,7 @@ if ( ! interface_exists( 'APIAPI\Core\Storages\Storage_Interface' ) ) {
 		 * @param string $key      The key to retrieve its value.
 		 * @return scalar|null The value, or null if not stored.
 		 */
-		public function get( $basename, $group, $key );
+		public function retrieve( $basename, $group, $key );
 
 		/**
 		 * Retrieves multiple values.
@@ -65,11 +65,11 @@ if ( ! interface_exists( 'APIAPI\Core\Storages\Storage_Interface' ) ) {
 		 *
 		 * @param string $basename The basename under which to store.
 		 * @param string $group    The group identifier of the group in which to store.
-		 * @param array  $keys     Array of keys.
+		 * @param array  $keys     The keys to retrieve their values.
 		 * @return array Associative array of `$key => $value`. The $value might is null, if
 		 *               none is stored.
 		 */
-		public function get_multi( $basename, $group, $keys );
+		public function retrieve_multi( $basename, $group, $keys );
 
 		/**
 		 * Deletes a single value.
@@ -91,7 +91,7 @@ if ( ! interface_exists( 'APIAPI\Core\Storages\Storage_Interface' ) ) {
 		 *
 		 * @param string $basename The basename under which to store.
 		 * @param string $group    The group identifier of the group in which to store.
-		 * @param array  $keys     Array of keys.
+		 * @param array  $keys     The keys to delete their values.
 		 */
 		public function delete_multi( $basename, $group, $keys );
 	}
